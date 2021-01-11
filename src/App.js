880e7939ff1cf5cf8e018619af6e1f0aacb6e774
elvynmejia <elvynmejia@gmail.com>
2021-01-11 03:06:04 +0000

    Adds grid layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector} from 'react-redux';
-
+import Grid from '@material-ui/core/Grid';
 
 import Leads from 'components/leads/leads';
 import ToastMessage from 'components/shared/toast_message'
@@ -12,10 +12,16 @@ const App = (props) => {
   });
 
   return (
-    <>
-      <ToastMessage {...toastProps } />
-      <Leads />
-    </>
+    <Grid container spacing={2}>
+      <Grid item xs={2}>
+      </Grid>
+      <Grid item xs={8}>
+        <ToastMessage {...toastProps } />
+        <Leads />
+      </Grid>
+      <Grid item xs={2}>
+      </Grid>
+    </Grid>
   );
 };
 
