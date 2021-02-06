5e63868a13c973206a9c01e5551d237415f1e2bb
Elvyn Mejia <elvynmejia@gmail.com>
2021-02-06 05:14:09 +0000

    Fixes lint errors

--- src/components/leads/leads.jsx
+++ src/components/leads/leads.jsx
@@ -1,30 +1,20 @@
 import React, { useEffect }from 'react';
-import { get } from 'lodash';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+import Chip from '@material-ui/core/Chip';
 
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import Chip from '@material-ui/core/Chip';
-import Button from '@material-ui/core/Button';
-
-import Divider from '@material-ui/core/Divider';
-
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
 import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
 
 
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 import BusinessIcon from '@material-ui/icons/Business';
 
 import { findAll } from 'actions/api';
@@ -32,17 +22,12 @@ import { openModal } from 'actions/interfaces';
 
 import { TYPE as LEAD_TYPE } from 'models/lead';
 
-import AddStageForm from './add_stage';
+// import AddStageForm from './add_stage';
 import AddLead from './add_lead';
 import { Sequence } from './sequence';
-import ModalDialog from 'components/shared/modal_dialog';
-import Details from 'components/stages/details';
+import StageDetails from 'components/stages/details_modal';
+import { KEY } from './index';
 
-import { KEY, MODAL_ID } from './index';
-
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import ButtonBase from '@material-ui/core/ButtonBase';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -95,14 +80,6 @@ export const Leads = () => {
       </Fab>
       <AddLead open={true} modalId={ADD_NEW_LEAD_MODAL_ID} />
       {leads.map(({id: lead_id, company_name, role, status, description, current_stage_id }) => {
-        const props = {
-          lead_id,
-          company_name,
-          role,
-          status,
-          description,
-        };
-
         return (
           <Card key={lead_id} className={classes.root} variant="outlined">
             <Grid container className={classes.root} spacing={2}>
@@ -146,12 +123,9 @@ export const Leads = () => {
                           >
                             Current Stage: {current_stage_id}
                           </Link>
-                          <ModalDialog
+                          <StageDetails
+                            stageId={current_stage_id}
                             modalId={STAGE_DETAILS_MODAL_ID}
-                            title="Current Stage"
-                            maxWidth="lg"
-                            content={<Details stageId={current_stage_id} />}
-                            primaryAction={() => console.log('primary action')}
                           />
                         </Typography>
                       </>
@@ -185,34 +159,9 @@ export const Leads = () => {
                 </Grid>
               </Grid>
             </Grid>
-
             <Grid>
               <Sequence lead_id={lead_id} />
             </Grid>
-            {/*<ComplexGrid
-              {...props }
-            >
-              <CardContent>
-                <Journey lead_id={lead_id} />
-              </CardContent>
-              <CardActions disableSpacing>
-                <IconButton
-                  onClick={() => addStage(lead_id)}
-                  aria-expanded={collapseAddStage[lead_id]}
-                  aria-label="add stage"
-                >
-                  <AddIcon />
-                </IconButton>
-                <IconButton
-                  onClick={() => seeMore(lead_id)}
-                  aria-expanded={collapseSeeMore[lead_id]}
-                  aria-label="show more"
-                >
-                  {collapseSeeMore[lead_id] ? <ExpandLess /> : <ExpandMore />}
-                </IconButton>
-              </CardActions>
-              <AddStageForm open={collapseAddStage[lead_id]} lead_id={lead_id} />
-            </ComplexGrid> */}
           </Card>
         )
       })}
