ab75f6414cda6e7d2ac2a414384c0572be28c667
Elvyn Mejia <elvynmejia@gmail.com>
2021-02-09 17:37:29 -0800

    Builds state options

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,4 +8,9 @@ export const STATES = {
   'not_a_good_fit': 'Not a good fit', // for me or for them
 }
 
+export const stateOptions = Object.keys(STATES).map(k => ({
+  label: STATES[k],
+  value: k,
+}));
+
 export default STATES;
