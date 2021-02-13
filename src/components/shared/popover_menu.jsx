a9accc7ddea7b972c346a4f91b1e0d8ebee12069
Elvyn Mejia <elvynmejia@gmail.com>
2021-02-13 01:55:53 +0000

    Stores synthetic event on local state

--- src/components/shared/popover_menu.jsx
+++ src/components/shared/popover_menu.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PropTypes as T } from 'prop-types';
 
 import { useDispatch, useSelector } from 'react-redux';
@@ -12,12 +12,35 @@ import { closeMenu, openMenu } from 'actions/interfaces';
 export const PopoverMenu = ({ menuId, children, ...rest }) => {
   const dispatch = useDispatch();
 
-  const {
-    open,
-    target,
-  } = useSelector(state => (
+  // redux crashes if we try to store a syntetic event
+  // so we need this little hack
+  const [anchorElements, setAnchorEl] = useState({});
+
+  const onClick = ({ currentTarget }) => {
+    setAnchorEl({
+      ...anchorElements,
+      [menuId]: currentTarget,
+    });
+
+    dispatch(
+      openMenu({ target: menuId, id: menuId})
+    )
+  }
+
+  const onClose = () => {
+    setAnchorEl({
+      ...anchorElements,
+      [menuId]: null,
+    });
+
+    dispatch(closeMenu(menuId));
+  }
+
+  const { open } = useSelector(state => (
     state.interfaces?.menu?.[menuId] ?? false
   ));
+
+  const anchorEl = anchorElements[menuId];
 
   return (
     <>
@@ -25,23 +48,15 @@ export const PopoverMenu = ({ menuId, children, ...rest }) => {
         aria-label="more"
         aria-controls={menuId}
         aria-haspopup="true"
-        onClick={(e) => {
-          dispatch(
-            openMenu({ target: e.currentTarget, id: menuId})
-          )
-        }}
+        onClick={onClick}
       >
         <MoreVertIcon />
       </IconButton>
       <Menu
         id={menuId}
-        anchorEl={target}
-        open={open}
-        onClose={() => dispatch(closeMenu(menuId))}
-        anchorOrigin={{
-          vertical: 'center',
-          horizontal: 'left',
-        }}
+        anchorEl={anchorEl}
+        open={open || false}
+        onClose={onClose}
         transformOrigin={{
           vertical: 'top',
           horizontal: 'right',
