f7af49ed7a6b99bca260aa26dedde4ed18fe557f
Elvyn Mejia <elvynmejia@gmail.com>
2021-02-19 10:08:32 -0800

    Adds basic navigation bar and side nav

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
 
 import cyan from '@material-ui/core/colors/cyan';
+import purple from '@material-ui/core/colors/purple';
+
 import red from '@material-ui/core/colors/red';
 
 import store from './store';
@@ -20,10 +22,25 @@ import reportWebVitals from './reportWebVitals';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 
+// import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
 const themeName = 'ontrail';
 
 const palette = {
-  primary: { main: cyan['600'], contrastText: '#FAFAFA' },
+  primary: { main: purple['600'], contrastText: '#FAFAFA' },
   secondary: { main: red['500'], contrastText: '#FAFAFA' },
 };
 
@@ -31,13 +48,95 @@ const theme = {
   ...createMuiTheme({ palette, themeName }),
 };
 
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerContainer: {
+    overflow: 'auto',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+export const NavBar = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            TraLent
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+            {/*
+              {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                <ListItem button key={text}>
+                  <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItem>
+              ))}
+            */}
+            <ListItem button key="leads">
+              <ListItemText primary="Leads" />
+            </ListItem>
+          </List>
+          <Divider />
+
+          {/*
+            <List>
+              {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                <ListItem button key={text}>
+                  <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItem>
+              ))}
+            </List>
+          */}
+        </div>
+      </Drawer>
+      <main className={classes.content}>
+        <Toolbar />
+        <App />
+      </main>
+    </div>
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
       <Provider store={store}>
         <Router>
           <ThemeProvider theme={theme}>
-            <App />
+            <NavBar />
           </ThemeProvider>
         </Router>
       </Provider>
