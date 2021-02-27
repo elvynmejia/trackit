82464030fd9911bfd2b69dbd8c661921ac9c445d
Elvyn Mejia <elvynmejia@gmail.com>
2021-02-27 15:45:33 -0800

    Creates NavBar component

---
+++ src/components/nav.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import Link from '@material-ui/core/Link';
+
+import ListItemLink from 'components/shared/list_item_link';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerContainer: {
+    overflow: 'auto',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  logo: {
+    color: 'white',
+    '&:hover': {
+      textDecoration: 'none',
+    },
+  },
+}));
+
+export const NavBar = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            <Link
+              component={RouterLink}
+              to="/"
+              exact
+              className={classes.logo}
+            >
+              Tralent
+            </Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+            <ListItem>
+              <ListItemLink
+                to="/leads"
+                exact primary="Leads"
+              />
+            </ListItem>
+          </List>
+          <Divider />
+        </div>
+      </Drawer>
+    </>
+  );
+}
+
+
+export default NavBar;
